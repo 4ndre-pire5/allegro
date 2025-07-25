c316d2f1048166c167979895e9e28fba385ae090
Andre Pires <erdnapfaserip@gmail.com>
2025-07-25 11:36:07 -0300

    criação arquivo .c e .cpp

--- Teste/main_cpp.cpp
+++ Teste/main_cpp.cpp
@@ -1,21 +1,21 @@
 #include <allegro5/allegro5.h>
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char **argv) {
     if (!al_init()) {
-        fprintf(stderr, "Falha ao iniciar a Allegro.\n");
+        std::cerr << "Falha ao iniciar a Allegro." << std::endl;
         return 1;
     }
 
-    ALLEGRO_DISPLAY *display = NULL;
-    display = al_create_display(640, 480);
+    ALLEGRO_DISPLAY *display = nullptr;
+    display = al_create_display(1024, 768);
     if (!display) {
-        fprintf(stderr, "Falha ao criar o display.\n");
+        std::cerr << "Falha ao criar o display" << std::endl;
         al_destroy_display(display); // Certifique-se de destruir o display em caso de falha.
         return 1;
     }
 
-    fprintf(stdout, "Allegro e display iniciados com sucesso!\n");
+    std::cout << "Allegro e display iniciados com sucesso!" << std::endl;
 
     al_rest(5.0); // Espera 5 segundos
 
